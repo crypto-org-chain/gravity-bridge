fe21f419fafb6e7c88b2d924ef9b3e2316925e28
HuangYi <huang@crypto.com>
2022-02-07 17:58:20 +0800

    fix cli sign-delegate-keys

--- orchestrator/gorc/src/commands/sign_delegate_keys.rs
+++ orchestrator/gorc/src/commands/sign_delegate_keys.rs
@@ -7,7 +7,6 @@ use std::time::Duration;
 /// Sign delegate keys command
 #[derive(Command, Debug, Default, Clap)]
 pub struct SignDelegateKeysCmd {
-    #[clap(short, long)]
     pub args: Vec<String>,
 }
 
