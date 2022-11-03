dd2abf28b60696975f21309bc02f5131734215b4
Thomas Nguy <thomas.nguy@crypto.com>
2022-11-03 13:50:56 +0900

    fix test

--- integration_tests/valset_stress_test.go
+++ integration_tests/valset_stress_test.go
@@ -15,10 +15,12 @@ import (
 /// Write test_valset_update test to get latest nonce value
 func (s *IntegrationTestSuite) TestValsetStressUpdate() {
 	s.Run("Bring up chain, and test the valset update", func() {
-		orchKey := s.chain.orchestrators[1]
-		keyring := orchKey.keyring
+		orch := s.chain.orchestrators[1]
+		keyring := orch.keyring
 
-		clientCtx, err := s.chain.clientContext("tcp://localhost:26657", keyring, "orch", orchKey.keyInfo.GetAddress())
+		orchAddress, err := s.chain.orchestrators[1].keyInfo.GetAddress()
+		s.Require().NoError(err)
+		clientCtx, err := s.chain.clientContext("tcp://localhost:26657", keyring, "orch", orchAddress)
 		s.Require().NoError(err)
 
 		startingNonce, err := s.getLastValsetNonce(gravityContract)
@@ -27,16 +29,17 @@ func (s *IntegrationTestSuite) TestValsetStressUpdate() {
 		bondTokens := sdk.TokensFromConsensusPower(50000, sdk.DefaultPowerReduction)
 		bondCoin := sdk.NewCoin("testgb", bondTokens)
 
-		delegator := s.chain.orchestrators[1].keyInfo.GetAddress()
-		val := sdk.ValAddress(s.chain.validators[3].keyInfo.GetAddress())
+		valAddress, err := s.chain.validators[3].keyInfo.GetAddress()
+		s.Require().NoError(err)
+		val := sdk.ValAddress(valAddress)
 
 		// Delegate about 5% of the total staking power.
 		s.Require().Eventuallyf(func() bool {
 			s.T().Logf("Sending in valset request (starting_eth_valset_nonce %d)", startingNonce)
 
-			s.T().Logf("Delegating %v to %v in order to generate a validator set update", bondCoin, delegator)
+			s.T().Logf("Delegating %v to %v in order to generate a validator set update", bondCoin, orchAddress)
 
-			delegate := types.NewMsgDelegate(delegator, val, bondCoin)
+			delegate := types.NewMsgDelegate(orchAddress, val, bondCoin)
 			response, err := s.chain.sendMsgs(*clientCtx, delegate)
 			if err != nil {
 				s.T().Logf("error: %s", err)
@@ -58,7 +61,7 @@ func (s *IntegrationTestSuite) TestValsetStressUpdate() {
 
 			s.Require().NoError(err, "error querying delegator bonded validators")
 			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.Delegation(context.Background(), &types.QueryDelegationRequest{DelegatorAddr: delegator.String(), ValidatorAddr: val.String()})
+			res, err := queryClient.Delegation(context.Background(), &types.QueryDelegationRequest{DelegatorAddr: orchAddress.String(), ValidatorAddr: val.String()})
 			if err != nil {
 				s.T().Logf("error: %s", err)
 				return false
