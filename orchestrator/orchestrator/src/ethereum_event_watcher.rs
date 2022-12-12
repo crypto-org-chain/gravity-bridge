b72ca848ac0f3def19c6cb71d4aa2722ec3cf030
Thomas Nguy <thomas.nguy@crypto.com>
2022-12-12 13:30:15 +0900

    add log

--- orchestrator/orchestrator/src/ethereum_event_watcher.rs
+++ orchestrator/orchestrator/src/ethereum_event_watcher.rs
@@ -42,7 +42,7 @@ pub async fn check_for_events<S: Signer + 'static, CS: CosmosSigner>(
     let prefix = contact.get_prefix();
     let our_cosmos_address = cosmos_key.to_address(&prefix).unwrap();
     let mut latest_block = get_block_number_with_retry(eth_client.clone()).await;
-    if block_delay >= latest_block {
+    if latest_block >= block_delay {
         latest_block = latest_block - block_delay;
     } else {
         latest_block = U64::zero();
@@ -75,6 +75,8 @@ pub async fn check_for_events<S: Signer + 'static, CS: CosmosSigner>(
         .event(&ValsetUpdatedEventFilter::abi_signature());
 
     let search_range = starting_block..ending_block;
+
+    info!("check_for_events from {:?} to {:?}", starting_block, ending_block);
 
     // select uses an inclusive version of the range
     erc20_deployed_filter = erc20_deployed_filter.select(search_range.clone());
