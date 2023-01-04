0130592333141cf0175d6c597c67688ca79d1624
Thomas Nguy <thomas.nguy@crypto.com>
2023-01-04 15:38:23 +0900

    make erc20 mod private

--- orchestrator/gravity_abi/src/erc20.rs
+++ orchestrator/gravity_abi/src/erc20.rs
@@ -1,7 +1,7 @@
 pub use erc20::*;
 #[allow(clippy::too_many_arguments, non_camel_case_types)]
 #[allow(clippy::module_inception)]
-pub mod erc20 {
+mod erc20 {
     #![allow(clippy::enum_variant_names)]
     #![allow(dead_code)]
     #![allow(clippy::type_complexity)]
