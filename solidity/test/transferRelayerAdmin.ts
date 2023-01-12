241e2f035375002bc0c021f5d30a77fb91d15837
Thomas Nguy <thomas.nguy@crypto.com>
2023-01-12 11:45:57 +0900

    add more flexibility for control

--- solidity/test/transferRelayerAdmin.ts
+++ solidity/test/transferRelayerAdmin.ts
@@ -16,7 +16,7 @@ const { expect } = chai;
 
 
 async function runTest(opts: {
-    isRelayerAdmin?: boolean;
+    isAdmin?: boolean;
 }) {
 
 
@@ -35,22 +35,22 @@ async function runTest(opts: {
     } = await deployContracts(gravityId, validators, powers, powerThreshold);
 
 
-    if (!opts.isRelayerAdmin) {
-        await gravity.connect(signers[1]).transferRelayerAdmin(signers[1].address);
+    if (!opts.isAdmin) {
+        await gravity.connect(signers[1]).transferAdmin(signers[1].address);
     }
 
 }
 
 describe("transferRelayerAdmin tests", function () {
-    it("Only relayerAdmin can call transferRelayerAdmin", async function () {
+    it("non admin cannot call transferAdmin", async function () {
         await expect(runTest({
-            isRelayerAdmin: false
-        })).to.be.revertedWith("Ownable: caller is not the owner");
+            isAdmin: false
+        })).to.be.revertedWith("AccessControl: account 0xead9c93b79ae7c1591b1fb5323bd777e86e150d4 is missing role 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42");
     });
 
-    it("old relayAdmin have no role after transferred", async function () {
+    it("admin can call transferAdmin", async function () {
         await expect(runTest({
-            isRelayerAdmin: true
-        })).to.be.revertedWith("Ownable: caller is not the owner");
+            isAdmin: true
+        }))
     });
 });
